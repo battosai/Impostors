df9faf2e71093f9faf08bd2c72e9141edc594e08
Brian Tsai <bt92130@gmail.com>
2018-02-01 00:18:12 -0500

    added head images, started grid script

---
+++ Scripts/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+	public float offset {get{return 10.5f;}}
+	//make an array of 9 to randomly decide which head goes where on the grid
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
