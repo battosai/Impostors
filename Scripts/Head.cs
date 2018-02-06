e43a1e3cab3487c83fcb0b649755bc5ac2209489
Brian Tsai <bt92130@gmail.com>
2018-02-06 00:35:16 -0500

    unity 2017.3 update, grid class to gridd

--- Scripts/Head.cs
+++ Scripts/Head.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Head : Grid
+public class Head : Gridd
 {
 	//public attributes
-	public bool isDefector;// {get; set;}
+	public bool isDefector {get; set;}
 	public GameObject selectedBorderClone {get; set;}
 	public GameObject deadClone {get; set;}
 	//public attributes, but only changeable within class
@@ -13,7 +13,7 @@ public class Head : Grid
 	public bool isSelected {get; private set;}
 	public Collider2D coll {get; private set;}
 	//private read-only constants
-	private float defaultSelectedBorderZ {get{return -9.5f;}}
+	private float defaultCloneZ {get{return -1f;}}
 
 	void Awake()
 	{
@@ -41,23 +41,23 @@ public class Head : Grid
 	//when moving to next round, gamestate calls on all selectedheads
 	public void toggleSelected()
 	{
-		if(!isSelected && Grid.selectedHeads.Count == GameState.selectCount[GameState.gameRound])
+		if(!isSelected && Gridd.selectedHeads.Count == GameState.selectCount[GameState.gameRound])
 		{
-			Debug.Log("selected: " + Grid.selectedHeads.Count + " Max: " + GameState.selectCount[GameState.gameRound]);
+			Debug.Log("selected: " + Gridd.selectedHeads.Count + " Max: " + GameState.selectCount[GameState.gameRound]);
 			return;
 		}
 		isSelected = !isSelected;
-		int index = Grid.heads.IndexOf(gameObject);
+		int index = Gridd.heads.IndexOf(gameObject);
 		if(isSelected && selectedBorderClone == null)
 		{
-			Grid.selectedHeads.Add(Grid.heads[index]);
-			selectedBorderClone = Instantiate(Grid.selectedBorderSprite);
+			Gridd.selectedHeads.Add(Gridd.heads[index]);
+			selectedBorderClone = Instantiate(Gridd.selectedBorderSprite);
 			selectedBorderClone.GetComponent<SpriteRenderer>().enabled = true;
-			selectedBorderClone.transform.position = transform.position;
+			selectedBorderClone.transform.position = new Vector3(transform.position.x, transform.position.y, defaultCloneZ);
 		}
 		else if(!isSelected && selectedBorderClone != null)
 		{
-			Grid.selectedHeads.Remove(Grid.heads[index]);
+			Gridd.selectedHeads.Remove(Gridd.heads[index]);
 			Destroy(selectedBorderClone);
 		}
 		else
@@ -71,14 +71,14 @@ public class Head : Grid
 		//do later, make restart button first
 		if(selectedBorderClone != null)
 		{
-			int index = Grid.heads.IndexOf(gameObject);
+			int index = Gridd.heads.IndexOf(gameObject);
 			Debug.Log("Heads[" + index + "] is still selected");
-			Grid.selectedHeads.Remove(Grid.heads[index]);
+			Gridd.selectedHeads.Remove(Gridd.heads[index]);
 			Destroy(selectedBorderClone);
 		}
-		deadClone = Instantiate(Grid.deadSprite);
+		deadClone = Instantiate(Gridd.deadSprite);
 		deadClone.GetComponent<SpriteRenderer>().enabled = true;
-		deadClone.transform.position = transform.position;
+		deadClone.transform.position = new Vector3(transform.position.x, transform.position.y, defaultCloneZ);
 		coll.enabled = false;
 		isDead = true;
 	}
